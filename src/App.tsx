8809f82fea95dd24750e3bddf113a8db0a3990ee
João Pedro <joao21dev@gmail.com>
2022-08-13 17:43:07 -0300

    home page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import CartContainer from './components/cartContainer/CartContainer';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div>
-      <CartContainer />
+      <Home />
     </div>
   );
 }
